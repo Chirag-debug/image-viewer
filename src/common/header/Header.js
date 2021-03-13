983c02ec6706408915b4e2044658018451e40670
Chirag-debug <mulla.chirag@gmail.com>
2021-03-13 10:14:16 +0530

    folder structure & header

---
+++ src/common/header/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="HeaderBar">
+                <div className="HeaderTitle">
+                Image Viewer
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
